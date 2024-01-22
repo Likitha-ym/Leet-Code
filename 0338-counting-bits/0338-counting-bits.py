131e087b14c1733eb8261aa468646c2a5520c474
Likitha. YM <132182508+Likitha-ym@users.noreply.github.com>
2024-01-22 20:59:54 +0530

    Time: 45 ms (84.98%), Space: 15.6 MB (93.47%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def countBits(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        result = [0] * (n + 1)
+        
+        for i in range(1, n + 1):
+            result[i] = result[i // 2] + (i % 2)
+        
+        return result
