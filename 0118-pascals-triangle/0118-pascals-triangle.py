21da0c2a6976830b470b05e2b4af56c5ab67fb77
Likitha. YM <132182508+Likitha-ym@users.noreply.github.com>
2024-01-24 02:42:39 +0530

    Time: 19 ms (39.92%), Space: 11.7 MB (96.66%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        result = [[1]]
+        
+        for i in range(1, numRows):
+            row = [1] + [result[i-1][j-1] + result[i-1][j] for j in range(1, i)] + [1]
+            result.append(row)
+        
+        return result
