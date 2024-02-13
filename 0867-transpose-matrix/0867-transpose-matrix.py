432421c6bbf8b7d4de068a9dc7bb89371aaf07c1
Likitha. YM <132182508+Likitha-ym@users.noreply.github.com>
2024-02-13 23:34:09 +0530

    Time: 43 ms (82.29%), Space: 12.5 MB (87.50%) - LeetHub

---
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def transpose(self, matrix):
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        transposed = [[0 for _ in range(m)] for _ in range(n)]
+        
+        for i in range(m):
+            for j in range(n):
+                transposed[j][i] = matrix[i][j]
+        
+        return transposed
